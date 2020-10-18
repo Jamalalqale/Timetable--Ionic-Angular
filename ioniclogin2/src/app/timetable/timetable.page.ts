c1ab8c89ae6bdf732989140c89aae3f9c2f8e8f7
Jamal Alqale <libyan.joey@gmail.com>
2020-10-18 15:39:42 +0200

    Create timetable.page.ts

---
+++ ioniclogin2/src/app/timetable/timetable.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable',
+  templateUrl: './timetable.page.html',
+  styleUrls: ['./timetable.page.scss'],
+})
+export class TimetablePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
