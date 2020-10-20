3e4aedf264cb795413870ef3e79d4280d5cf1ff3
Jamal Alqale <libyan.joey@gmail.com>
2020-10-20 14:45:39 +0200

    Update home.page.ts

--- ioniclogin2/src/app/home/home.page.ts
+++ ioniclogin2/src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { PostProvider } from '../providers/post-provider';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/Storage';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public items= [];
+  constructor(
+    private router: Router,
+    public postPvdr: PostProvider,
+    private storage: Storage,
+  	public toastCtrl: ToastController
 
-  constructor() {}
+  ) {}
+
+  ngOnInit() {
+
+
+  }
+
+
+  timetable(){
+  	this.router.navigate(['/timetable']);
+  }
+
+  news(){
+  	this.router.navigate(['/news']);
+  }
+
+
+profile(){
+  this.router.navigate(['/profile']);
+}
 
 }
