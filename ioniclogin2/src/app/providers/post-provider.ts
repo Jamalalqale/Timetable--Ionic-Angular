e07994d30c92a45f80683452500c959f4db68718
Jamal Alqale <libyan.joey@gmail.com>
2020-10-20 14:46:22 +0200

    Update post-provider.ts

--- ioniclogin2/src/app/providers/post-provider.ts
+++ ioniclogin2/src/app/providers/post-provider.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { RequestOptions } from '@angular/http';
+
 
 
 import 'rxjs/add/operator/map';
