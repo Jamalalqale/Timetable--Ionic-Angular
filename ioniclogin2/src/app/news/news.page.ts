474ac114c94e7e42614273fa57f860766d055ab1
Jamal Alqale <libyan.joey@gmail.com>
2020-10-20 14:46:00 +0200

    Create news.page.ts

---
+++ ioniclogin2/src/app/news/news.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { PostProvider } from '../providers/post-provider';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/Storage';
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.page.html',
+  styleUrls: ['./news.page.scss'],
+})
+export class NewsPage implements OnInit {
+  public items= [];
+
+
+
+  constructor(
+    private router: Router,
+    public postPvdr: PostProvider,
+    private storage: Storage,
+  	public toastCtrl: ToastController
+
+  ) {}
+
+  ngOnInit() {
+
+    let body = {
+      //username: 'y',
+      //password: 'y',
+      //aksi: 'getdata'
+    };
+
+
+    this.postPvdr.postData(body, 'get_news.php').subscribe((data: any) => {
+        
+   
+    /*  for(let datas of data.result){
+        this.items.push(datas);
+      }*/
+
+      this.items=data.result;
+
+      console.log(data.result);
+    
+    });
+  }
+
+
+  
+
+   
+
+
+}
